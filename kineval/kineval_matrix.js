70d8e4bdf91ca170d662bfdb219628d34ef7571f
Zheming Zhou <zhezhou@umich.edu>
2020-09-22 13:41:05 -0400

    Factorize kineval stencil for FK problems, fix bugs in previous version

--- kineval/kineval_matrix.js
+++ kineval/kineval_matrix.js
@@ -31,57 +31,63 @@ function matrix_copy(m1) {
 //   generate_rotation_matrix_Y
 //   generate_rotation_matrix_Z
 
-function matrix_multiply(m1,m2) {
-    // returns 2D array that is the result of m1*m2
 
-}
 
-function matrix_transpose(m) {
-    // returns 2D array that is the result of m1*m2
+// **** Function stencils are provided below, please uncomment and implement them ****//
 
-}
 
-function matrix_pseudoinverse(m) {
-    // returns pseudoinverse of matrix m
 
-}
+// function matrix_multiply(m1,m2) {
+//     // returns 2D array that is the result of m1*m2
 
-function matrix_invert_affine(m) {
-    // returns 2D array that is the invert affine of 4-by-4 matrix m
+// }
 
-}
+// function matrix_transpose(m) {
+//     // returns 2D array that is the result of m1*m2
 
-function vector_normalize(v) {
-    // returns normalized vector for v
+// }
+
+// function matrix_pseudoinverse(m) {
+//     // returns pseudoinverse of matrix m
+
+// }
+
+// function matrix_invert_affine(m) {
+//     // returns 2D array that is the invert affine of 4-by-4 matrix m
+
+// }
+
+// function vector_normalize(v) {
+//     // returns normalized vector for v
     
-}
+// }
 
-function vector_cross(a,b) {
-    // return cross product of vector a and b with both has 3 dimensions
+// function vector_cross(a,b) {
+//     // return cross product of vector a and b with both has 3 dimensions
     
-}
+// }
 
-function generate_identity() {
-    // returns 4-by-4 2D array of identity matrix
+// function generate_identity() {
+//     // returns 4-by-4 2D array of identity matrix
     
-}
+// }
 
-function generate_translation_matrix(tx, ty, tz) {
-    // returns 4-by-4 matrix as a 2D array
+// function generate_translation_matrix(tx, ty, tz) {
+//     // returns 4-by-4 matrix as a 2D array
     
-}
+// }
 
-function generate_rotation_matrix_X(angle) {
-    // returns 4-by-4 matrix as a 2D array, angle is in radians
+// function generate_rotation_matrix_X(angle) {
+//     // returns 4-by-4 matrix as a 2D array, angle is in radians
     
-}
+// }
 
-function generate_rotation_matrix_Y(angle) {
-    // returns 4-by-4 matrix as a 2D array, angle is in radians
+// function generate_rotation_matrix_Y(angle) {
+//     // returns 4-by-4 matrix as a 2D array, angle is in radians
     
-}
+// }
 
-function generate_rotation_matrix_Z(angle) {
-    // returns 4-by-4 matrix as a 2D array, angle is in radians
+// function generate_rotation_matrix_Z(angle) {
+//     // returns 4-by-4 matrix as a 2D array, angle is in radians
     
-}
+// }
--- kineval/kineval_quaternion.js
+++ kineval/kineval_quaternion.js
@@ -8,26 +8,28 @@
 //   quaternion_to_rotation_matrix
 //   quaternion_multiply
 
-kineval.quaternionFromAxisAngle = function quaternion_from_axisangle(axis,angle) {
-    // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
-    var q = {};
+// **** Function stencils are provided below, please uncomment and implement them ****//
 
-}
+// kineval.quaternionFromAxisAngle = function quaternion_from_axisangle(axis,angle) {
+//     // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
+//     var q = {};
 
-kineval.quaternionNormalize = function quaternion_normalize(q1) {
-    // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
-    var q = {};
+// }
 
-}
+// kineval.quaternionNormalize = function quaternion_normalize(q1) {
+//     // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
+//     var q = {};
 
-kineval.quaternionMultiply = function quaternion_multiply(q1,q2) {
-    // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
-    var q = {};
+// }
+
+// kineval.quaternionMultiply = function quaternion_multiply(q1,q2) {
+//     // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
+//     var q = {};
     
 
-}
+// }
 
-kineval.quaternionToRotationMatrix = function quaternion_to_rotation_matrix (q) {
-    // returns 4-by-4 2D rotation matrix
+// kineval.quaternionToRotationMatrix = function quaternion_to_rotation_matrix (q) {
+//     // returns 4-by-4 2D rotation matrix
 
-}
+// }
